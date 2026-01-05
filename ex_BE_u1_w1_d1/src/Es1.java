d8119770f80ae03fb337d2b232f0bb75b3df1f07
Elisabetta Piacquadio <betta.pcq@gmail.com>
2026-01-05 15:05:37 +0100

    add new project

--- ex_BE_u1_w1_d1/src/Es1.java
+++ ex_BE_u1_w1_d1/src/Es1.java
@@ -1,13 +1,13 @@
-
 public class Es1 {
     public static void main(String[] args) {
-
-
         int product = mult(4, 6);
         System.out.println(product);
 
     }
 
-    private static int mult(int num1, int num2) { return num1 * num2; }
+    private static int mult(int num1, int num2) {
+        return num1 * num2;
     }
+
 }
+
